811e4044d27e05679e7e72abd3597fd4a7239027
kdipto1 <kdipto@outlook.com>
2022-05-10 13:30:59 +0600

    banner added , banner image changed

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Banner from './Banner';
 
 const Home = () => {
   return (
-    <div>
-      <h2>home page</h2>
-    </div>
+    <>
+     <Banner/> 
+    </>
   );
 };
 
