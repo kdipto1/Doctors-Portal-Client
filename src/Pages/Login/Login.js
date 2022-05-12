292ad7ffc8d762a879b52d85ebba8ad100d8c744
kdipto1 <kdipto@outlook.com>
2022-05-12 13:05:48 +0600

    react hook form added, .env.local error fixed with restarting the app

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,68 @@
-import React from 'react';
+import React from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  if (user) {
+    console.log(user);
+  }
+  const onSubmit = (data) => {};
+
   return (
-    <div>
-      <h1>this is login page.</h1>
+    <div className="flex h-screen justify-center items-center">
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="text-center text-2xl font-bold">Login</h2>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text">What is your name?</span>
+              </label>
+              <input
+                type="email"
+                placeholder="Your Email"
+                className="input input-bordered w-full max-w-xs"
+                {...register("email", {
+                  required: {
+                    value: true,
+                    message:"Email is Required"
+                  },
+                  pattern: {
+                    value: /[A-Za-z]{3}/,
+                    message: "Provide a valid email address",
+                  },
+                })}
+              />
+              <label className="label">
+                <span className="label-text-alt">Alt label</span>
+              </label>
+            </div>
+
+            <input />
+            {errors.firstName?.type === "required" && "First name is required"}
+
+            <input {...register("lastName", { required: true })} />
+            {errors.lastName && "Last name is required"}
+
+            <input type="submit" />
+          </form>
+          <div className="divider">OR</div>
+          <button
+            onClick={() => signInWithGoogle()}
+            className="btn btn-outline"
+          >
+            Continue With Google
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
