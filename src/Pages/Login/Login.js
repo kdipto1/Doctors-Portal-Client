c8d0d8bb17f23274b23dd13ba3df7048e1dd9247
kdipto1 <kdipto@outlook.com>
2022-05-12 23:01:15 +0600

    login page redirect added , login error fixed

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,37 +1,50 @@
-import React from "react";
-import { useSignInWithEmailAndPassword, useSignInWithGoogle } from "react-firebase-hooks/auth";
+import React, { useEffect } from "react";
+import {
+  useSignInWithEmailAndPassword,
+  useSignInWithGoogle,
+} from "react-firebase-hooks/auth";
 import auth from "../../firebase.init";
 import { useForm } from "react-hook-form";
 import Loading from "../Shared/Loading";
-import { Link } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 
 const Login = () => {
   const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
-  const [signInWithEmailAndPassword, user, loading, error] =
-    useSignInWithEmailAndPassword(auth);
   const {
     register,
     formState: { errors },
     handleSubmit,
   } = useForm();
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
 
   let signInError;
+  const navigate = useNavigate();
+  const location = useLocation();
+  let from = location.state?.from?.pathname || "/";
+
+  useEffect(() => {
+    if (user || gUser) {
+      console.log(user || gUser);
+      navigate(from, { replace: true });
+    }
+  }, [user, gUser, from, navigate]);
+
   if (loading || gLoading) {
-    return <Loading/>
+    return <Loading></Loading>;
   }
+
   if (error || gError) {
     signInError = (
-      <p className="text-red-600">
-        <span>{error?.message || gError?.message}</span>
+      <p className="text-red-500">
+        <small>{error?.message || gError?.message}</small>
       </p>
     );
   }
-  if (user|| gUser) {
-    console.log(user|| gUser);
-  }
+
   const onSubmit = (data) => {
-    console.log(data);
-    signInWithEmailAndPassword(data.email, data.password)
+    // console.log(data);
+    signInWithEmailAndPassword(data.email, data.password);
   };
 
   return (
@@ -42,7 +55,7 @@ const Login = () => {
           <form onSubmit={handleSubmit(onSubmit)}>
             <div className="form-control w-full max-w-xs">
               <label className="label">
-                <span className="label-text">What is your name?</span>
+                <span className="label-text">Email</span>
               </label>
               <input
                 type="email"
@@ -55,18 +68,18 @@ const Login = () => {
                   },
                   pattern: {
                     value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
-                    message: "Provide a valid email address",
+                    message: "Provide a valid Email",
                   },
                 })}
               />
               <label className="label">
                 {errors.email?.type === "required" && (
-                  <span className="label-text-alt text-red-600">
+                  <span className="label-text-alt text-red-500">
                     {errors.email.message}
                   </span>
                 )}
                 {errors.email?.type === "pattern" && (
-                  <span className="label-text-alt text-red-600">
+                  <span className="label-text-alt text-red-500">
                     {errors.email.message}
                   </span>
                 )}
@@ -78,7 +91,7 @@ const Login = () => {
               </label>
               <input
                 type="password"
-                placeholder="Your Password"
+                placeholder="Password"
                 className="input input-bordered w-full max-w-xs"
                 {...register("password", {
                   required: {
@@ -87,18 +100,18 @@ const Login = () => {
                   },
                   minLength: {
                     value: 6,
-                    message: "Password have to be 6 character or longer",
+                    message: "Must be 6 characters or longer",
                   },
                 })}
               />
               <label className="label">
                 {errors.password?.type === "required" && (
-                  <span className="label-text-alt text-red-600">
+                  <span className="label-text-alt text-red-500">
                     {errors.password.message}
                   </span>
                 )}
                 {errors.password?.type === "minLength" && (
-                  <span className="label-text-alt text-red-600">
+                  <span className="label-text-alt text-red-500">
                     {errors.password.message}
                   </span>
                 )}
@@ -106,7 +119,6 @@ const Login = () => {
             </div>
 
             {signInError}
-
             <input
               className="btn w-full max-w-xs text-white"
               type="submit"
@@ -115,8 +127,8 @@ const Login = () => {
           </form>
           <p>
             <small>
-              New to doctors portal,{" "}
-              <Link className="text-secondary" to="/signup">
+              New to Doctors Portal{" "}
+              <Link className="text-primary" to="/signup">
                 Create New Account
               </Link>
             </small>
@@ -126,7 +138,7 @@ const Login = () => {
             onClick={() => signInWithGoogle()}
             className="btn btn-outline"
           >
-            Continue With Google
+            Continue with Google
           </button>
         </div>
       </div>
