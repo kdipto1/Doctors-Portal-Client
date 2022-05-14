642058919d1a91d280ec9bc79fe200c29a099189
kdipto1 <kdipto@outlook.com>
2022-05-14 12:21:43 +0600

    toastify package added

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -3,11 +3,11 @@ import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import auth from "../../firebase.init";
 
-const BookingModal = ({ treatment,date,setTreatment }) => {
+const BookingModal = ({ treatment, date, setTreatment }) => {
   const { _id, name, slots } = treatment;
   const [user, loading, error] = useAuthState(auth);
-  const formatDate = format(date, "PP")
-  const handleBooking = event => {
+  const formatDate = format(date, "PP");
+  const handleBooking = (event) => {
     event.preventDefault();
     const slot = event.target.slot.value;
     console.log(_id, name, slot);
@@ -18,11 +18,23 @@ const BookingModal = ({ treatment,date,setTreatment }) => {
       slot,
       patient: user.email,
       patientName: user.displayName,
-      phone : event.target.phone.value
-    }
+      phone: event.target.phone.value,
+    };
+    //post booking in api
+    fetch("http://localhost:5000/booking", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(booking),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        
+      });
     //to close the modal
-    setTreatment(null)
-  }
+    setTreatment(null);
+  };
   return (
     <div>
       <input type="checkbox" id="booking-modal" className="modal-toggle" />
@@ -37,17 +49,25 @@ const BookingModal = ({ treatment,date,setTreatment }) => {
           <h3 className="font-bold text-lg text-secondary">
             Booking for: {name}
           </h3>
-          <form onSubmit={handleBooking} className="grid grid-cols-1 gap-3 justify-items-center mt-2">
+          <form
+            onSubmit={handleBooking}
+            className="grid grid-cols-1 gap-3 justify-items-center mt-2"
+          >
             <input
               disabled
               type="text"
               value={format(date, "PP")}
               className="input input-bordered w-full max-w-xs"
             />
-            <select name="slot" className="select select-bordered w-full max-w-xs">
-              {
-                slots.map((slot,index) => <option key={index} value={slot}>{slot}</option>)
-              }
+            <select
+              name="slot"
+              className="select select-bordered w-full max-w-xs"
+            >
+              {slots.map((slot, index) => (
+                <option key={index} value={slot}>
+                  {slot}
+                </option>
+              ))}
             </select>
             <input
               type="text"
