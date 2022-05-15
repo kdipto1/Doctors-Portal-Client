1d4f89fc7de8ced434ef06deaa1f268a52c28549
kdipto1 <kdipto@outlook.com>
2022-05-15 13:22:37 +0600

    use token functionality added in login compo

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -7,6 +7,7 @@ import auth from "../../firebase.init";
 import { useForm } from "react-hook-form";
 import Loading from "../Shared/Loading";
 import { Link, useNavigate, useLocation } from "react-router-dom";
+import useToken from "../../Hooks/useToken";
 
 const Login = () => {
   const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
@@ -18,17 +19,19 @@ const Login = () => {
   const [signInWithEmailAndPassword, user, loading, error] =
     useSignInWithEmailAndPassword(auth);
 
+  const [token] = useToken(user || gUser);
+
   let signInError;
   const navigate = useNavigate();
   const location = useLocation();
   let from = location.state?.from?.pathname || "/";
-//  useeffect added for avoiding browser router error
+  //  useffect added for avoiding browser router error
   useEffect(() => {
-    if (user || gUser) {
+    if (token) {
       console.log(user || gUser);
       navigate(from, { replace: true });
     }
-  }, [user, gUser, from, navigate]);
+  }, [token, user, gUser, from, navigate]);
 
   if (loading || gLoading) {
     return <Loading></Loading>;
