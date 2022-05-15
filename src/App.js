293c33c6d6c4fb19e0d97787534c7c32538c212b
kdipto1 <kdipto@outlook.com>
2022-05-15 20:19:44 +0600

    require admin compo added , wrapped users in dashboard

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import MyAppointments from "./Pages/Dashboard/MyAppointments";
 import MyReview from "./Pages/Dashboard/MyReview";
 import MyHistory from "./Pages/Dashboard/MyHistory";
 import Users from "./Pages/Dashboard/Users";
+import RequireAdmin from "./Pages/Login/RequireAdmin";
 
 function App() {
   return (
@@ -38,10 +39,17 @@ function App() {
             </RequireAuth>
           }
         >
-          <Route index element={<MyAppointments/>}/>
-          <Route path="review" element={<MyReview/>}/>
-          <Route path="history" element={<MyHistory/>}/>
-          <Route path="users" element={<Users />}/>
+          <Route index element={<MyAppointments />} />
+          <Route path="review" element={<MyReview />} />
+          <Route path="history" element={<MyHistory />} />
+          <Route
+            path="users"
+            element={
+              <RequireAdmin>
+                <Users />
+              </RequireAdmin>
+            }
+          />
         </Route>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
