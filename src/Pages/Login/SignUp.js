5d2244910976cd6e9340cee90a95d5048ebef967
kdipto1 <kdipto@outlook.com>
2022-05-15 13:01:50 +0600

    use token compo added , added in signup , if user / guser , token navigates

--- src/Pages/Login/SignUp.js
+++ src/Pages/Login/SignUp.js
@@ -8,6 +8,7 @@ import auth from "../../firebase.init";
 import { useForm } from "react-hook-form";
 import Loading from "../Shared/Loading";
 import { Link, useNavigate } from "react-router-dom";
+import useToken from "../../Hooks/useToken";
 
 const SignUp = () => {
   const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
@@ -21,25 +22,26 @@ const SignUp = () => {
 
   const [updateProfile, updating, updateError] = useUpdateProfile(auth);
 
+  const [token] = useToken(user || gUser);
+
   const navigate = useNavigate();
 
   let signInError;
 
-  if (loading || gLoading|| updating) {
+  if (loading || gLoading || updating) {
     return <Loading></Loading>;
   }
 
-  if (error || gError|| updateError) {
+  if (error || gError || updateError) {
     signInError = (
       <p className="text-red-500">
-        <small>
-          {error?.message || gError?.message}
-        </small>
+        <small>{error?.message || gError?.message}</small>
       </p>
     );
   }
 
-  if (user || gUser) {
+  if (token) {
+    navigate("/appointment");
     // console.log(user || gUser);
   }
 
@@ -47,7 +49,6 @@ const SignUp = () => {
     await createUserWithEmailAndPassword(data.email, data.password);
     await updateProfile({ displayName: data.name });
     console.log("update done");
-    navigate("/appointment");
   };
   return (
     <div className="flex h-screen justify-center items-center">
