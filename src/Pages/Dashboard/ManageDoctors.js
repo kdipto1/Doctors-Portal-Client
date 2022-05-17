551823e7d9ac2c3da153fa2e16bb6d382f94790a
kdipto1 <kdipto@outlook.com>
2022-05-17 19:12:53 +0600

    Manage Doctors compo added

--- src/Pages/Dashboard/ManageDoctors.js
+++ src/Pages/Dashboard/ManageDoctors.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { useQuery } from "react-query";
 import Loading from "../Shared/Loading";
+import DoctorRow from "./DoctorRow";
 
 const ManageDoctors = () => {
-  const { data: doctors, isLoading } = useQuery("doctors", () =>
+  const { data: doctors, isLoading, refetch } = useQuery("doctors", () =>
     fetch("http://localhost:5000/doctor", {
       headers: {
         authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-      }
+      },
     }).then((res) => res.json())
   );
   if (isLoading) {
@@ -16,6 +17,24 @@ const ManageDoctors = () => {
   return (
     <div>
       <h2 className="text-2xl">Manage doctors:{doctors?.length}</h2>
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Avatar</th>
+              <th>Name</th>
+              <th>specialty</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              doctors.map((doctor,index) => <DoctorRow refetch={refetch} index={index} key={doctor._id} doctor={doctor}/>)
+            }
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
