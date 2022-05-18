46908a10b9411133275289cedbe355c2f7a725c8
kdipto1 <kdipto@outlook.com>
2022-05-18 13:29:42 +0600

    checkout form compo for stripe in Payment compo Elements added

--- src/Pages/Dashboard/Payment.js
+++ src/Pages/Dashboard/Payment.js
@@ -1,11 +1,54 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import React from "react";
+import { useQuery } from "react-query";
+import { useParams } from "react-router-dom";
+import Loading from "../Shared/Loading";
+import CheckoutForm from "./CheckoutForm";
 
+const stripePromise = loadStripe("pk_test_51L0gtQCleCCJSf6N0qVeujBBIvuXiFVg1yxwXQdrOAXXpdKUo162kw118PF7rvgnWE49F14H702XyEkS3qYBYoSD004hIsjWXd"
+);
 const Payment = () => {
   const { id } = useParams();
+  const url = `http://localhost:5000/booking/${id}`;
+  const {
+    data: appointment,
+    isLoading,
+    refetch,
+  } = useQuery(["booking", id], () =>
+    fetch(url, {
+      method: "GET",
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    }).then((res) => res.json())
+  );
+  if (isLoading) {
+    return <Loading />;
+  }
   return (
     <div>
-      <h2 className="text-2xl text-purple-600">Please pay for:{id}</h2>
+      <div class="card w-50 max-w-md bg-base-100 shadow-xl my-12">
+        <div class="card-body">
+          <p className="text-success font-bold">
+            Hello, {appointment.patientName}
+          </p>
+          <h2 class="card-title">Please Pay for {appointment.treatment}</h2>
+          <p>
+            Your Appointment:{" "}
+            <span className="text-orange-700">{appointment.date}</span> at{" "}
+            {appointment.slot}
+          </p>
+          <p>Please pay: ${appointment.price}</p>
+        </div>
+      </div>
+      <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
+        <div class="card-body">
+          <Elements stripe={stripePromise}>
+            <CheckoutForm  />
+          </Elements>
+        </div>
+      </div>
     </div>
   );
 };
